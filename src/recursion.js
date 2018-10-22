b8faa3d9e47a30b125097e5e4cec66f4dc8e0af7
seymakara <seymakara88@gmail.com>
2018-10-22 16:46:55 -0700

    solved the first 11 problems

--- src/recursion.js
+++ src/recursion.js
@@ -7,54 +7,317 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+	if(n < 0){
+		return null;
+	}
+	if (n === 0) {
+		return 1;
+	}
+	return (n * factorial(n-1));
 };
+//pass in 5
+//5 * 4 * 3 * 2 * 1
+// 5* fact(4)
+//		4 * fac(3)
+//			3 * fac(2)
+//					2* 1
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
+//input array
+//output return sum array
+//method iterate of the array, take value + next value
+//
+//1,2,3,4,5,6 - 6
+//1,2,3,4,5   - 5
+//1,2,3,4     - 4
+
 var sum = function(array) {
+	var newArray = array.slice();
+	if (newArray.length === 0){
+		return 0 ;
+	}
+	var lastItem = newArray.pop();
+	return (lastItem + sum(newArray));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
+//input nested array
+//output sum of the array
+//method flatten array
+// sum the array
+//
 var arraySum = function(array) {
+
+	var newArray = array.flat();
+
+	for(var i = 0; i < newArray.length; i++){
+       if(Array.isArray(newArray[i])) {
+		 var newArray = newArray.flat();
+	   }
+	}
+
+	if(newArray.length === 0){
+		return 0;
+	}
+	var lastItem = newArray.pop();
+	return(lastItem + arraySum(newArray));
+
 };
+
+// input  - number negative or positive - 
+// output - true or false - boolean
+// age case - array - flatten 
+// method  - if number is positive minus 2 until number === 0. if number is negative change numbers to positive
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	var posNum = Math.abs(n)
+	if(posNum === 0) {
+		console.log(true);
+		return true;
+	}else if(posNum === 1){ 
+		console.log(false);
+		return false;
+	}	
+	posNum = posNum - 2;
+	console.log(posNum)
+
+	return isEven(posNum)
 };
+
+//input - number
+//output - sum of all numbers below the number given
+//method - sum variable - input number - 1 until it is 0; add the number to the sum.
+//10 input
+// 10 - 1 = 9 --- sum(0) + 9 = 9
+//  9 - 1 = 8 --- sum(9) + 8 = 17
+//  8 - 1 = 7 --- sum(17) + 7 = 24
+// -1 -1 = -2
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+
+	if (n === 0 || n === 1) {
+		return 0;
+	}
+
+	if (n >= 2){
+	return n - 1 + sumBelow(n - 1)
+    }
+
+    if(n < 0){
+    	return n + 1 + sumBelow(n + 1)
+    }
+
 };
 
 // 6. Get the integers within a range (x, y).
-// range(2,9); // [3,4,5,6,7,8]
+// range(2,9); // [3,4,5,6,7,8] 
+// input - 2 numbers (x & y)
+// output - array of numbers between the 2 input numbers
+// method - 2 to 9
+// x + 1 push into array until x === y
+// 2,5
+// 2 + 1 = 3 - (is x === y) no - push into array [3]
+// 3 + 1 = 4 - (is x === y) no - push into array [3,4] 
+// 4 + 1 = 5 - (is x === y) yes -stop
+
+// 3,5  = return 4
+//x = 3, y = 5
+
+//3,6 = return 4,5
+
+//==================================================
+// input range(2,9)
+
+//steps   x  | y  |      array
+//step 1  2  | 9  |[]    []
+//step 2  3  | 9  |[3]   [3]
+//step 3  4  | 9  |[4]   [3, 4]
+//step 4  5  | 9  |[5]   [3, 4, 5]
+//step 5  6  | 9  |[6]   [3, 4, 5, 6]
+//step 6  7  | 9  |[7]   [3, 4, 5, 6, 7]
+//step 7  8  | 9  |[8]   [3, 4, 5, 6, 7, 8]
+//step 8  9  | 9  |      [3, 4, 5, 6, 7, 8] //return
+
+// x + 1 // y stays the same
+// x value goes into array
+// stop when x === y return array
+
+// range(2-5)
+//	 ||
+// range(2,4).concat(4) > [3].concat(4) > [3,4]
+//   ||
+// range(2,3).concat(3) > [3]
+//   || 
+//    [].concat(3) 
+
+
 var range = function(x, y) {
+
+    if(y - x === 1 || x - y === 1) {
+    	return [];
+    }
+
+	if (x === y ) {
+		return [];
+	}
+   	if (x < y){
+   		// console.log("y- before: ", y)
+		y = y-1
+		// console.log("y- after: ", y)
+		return range(x,y).concat(y)
+    }
+
+    if (x > y){
+    	// console.log("y+ before: ", y)
+		y = y+1
+		// console.log("y+ after: ", y)
+		return range(x,y).concat(y)
+    }
 };
+
 
 // 7. Compute the exponent of a number.
 // The exponent of a number says how many times the base number is used as a factor.
 // 8^2 = 8 x 8 = 64. Here, 8 is the base and 2 is the exponent.
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
+
+//input 2 number (base,exp)
+// output number - base multiple as many times as exp tells us
+// method - 
+// run function until exp is 0 - exp - 1;
+// if exp === 0 then result = 1; exponent(2,0)
+// 8 x 8 x 8
+//exponent(8,3)
+//exp = 2 | 8 * exponent(8,2) > 8 * 64 = 512
+//                 exp = 1 | 8 * exponent(8,1) > 64
+//                                  exp = 0 | 8 * exponent(8, 0) > 8
+//                                            8  *      1
+
+// exponent(5, -2) = 1/25
 var exponent = function(base, exp) {
+
+	if(exp === 0){
+		return 1
+	}
+    if (exp > 0){
+		exp = exp - 1;
+		return base * exponent(base,exp)
+    }
+    if (exp < 0) {
+    	exp = exp + 1;
+		return (1 / base * exponent(base,exp).toFixed(4))
+    }
 };
 
 // 8. Determine if a number is a power of two.
 // powerOfTwo(1); // true
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
+// input
+// output
+// method - multiply it by 2 devide by two and remaider is 0
+//if n === 1
+// return true
+//if n < 1
+//return false
+//
+//16 / 2 = 8
+//8 / 2 = 4
+//4 / 2 = 2
+//2 / 2 = 1
+
+//10 / 2 = 5
+//5 / 2 = 2.5
+//2.5 / 2 = 1.25
+//1.25 / 2 = 0.625
 var powerOfTwo = function(n) {
+
+	if(n === 1){
+       return true;
+	}
+
+	if (n < 1) {
+		return false;
+	}
+
+	n = n / 2;
+
+	return powerOfTwo(n)
+
+
 };
 
 // 9. Write a function that reverses a string.
+//input - string
+//out put - string
+//method - 
+// hello - 
+// step 1 | hello | o | 'o'
+// step 2 | hell  | l | 'ol'
+// step 2 | hel   | l | 'oll'
+// step 3 | he    | e | 'olle'
+// step 4 | h     | h | 'olleh'
 var reverse = function(string) {
+	if (string.length === 0){
+		return '';
+	}
+
+	
+	var i = string.length - 1
+	// console.log(i)
+	var newLetter = string[i]
+	// console.log("newLetter: ", newLetter)
+	var newString = string.slice(0, -1)
+	// console.log("newString: ", newString)
+
+	return newLetter.concat(reverse(newString))
 };
 
 // 10. Write a function that determines if a string is a palindrome.
+//input - string
+//output - true or false
+//method - take string compare 1 and last, until reach middel
+//(madam)
+//m = m
+//a = a
+// d middle - stop
 var palindrome = function(string) {
+
+	string = string.toLowerCase()
+	string = string.replace(/ /g,'');
+
+	// console.log(string)
+
+	if(string.length === 0 || string.length === 1){
+		return true
+	}
+
+	var lastLetter = string.slice(string.length - 1)
+	// console.log('lastLetter :', lastLetter)
+	var firstLetter = string.slice(0, 1)
+	// console.log('firstLetter :', firstLetter)
+	var newString = string.slice(0,-1)
+	// console.log('newString :', newString)
+	var newString = newString.slice(1)
+	// console.log('newString :', newString)
+
+
+	if(firstLetter === lastLetter) {
+		return palindrome(newString)
+
+	}else{ 
+		return false;
+	}
+
+
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
@@ -62,7 +325,23 @@ var palindrome = function(string) {
 // modulo(5,2) // 1
 // modulo(17,5) // 2
 // modulo(22,6) // 4
+// 5 % 2 = 2 + 2 remainer 1
+//17 % 5 = 5 + 5 + 5 remainder 2
+//22 % 6 = 6 + 6 + 6 remainder 4
+// 5 - 2 = 3
+// 3 - 2 = 1
+
 var modulo = function(x, y) {
+
+	if (x < y) {
+		return x;
+	}
+	if (x === 0 && y === 0){
+		return NaN;
+	}
+	
+	x = x - y;
+	return modulo(x,y)
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
